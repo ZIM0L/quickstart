d55ddb82403599639238d6b74754e3aed3c938a3
Adrian Z <sylzyg555@interia.pl>
2024-01-21 15:03:25 +0100

    test

--- web/main.dart
+++ web/main.dart
@@ -3,19 +3,23 @@ import 'dart:async';
 
 void main() {
   querySelector('#output')?.text = 'Gra w życie.';
+  List<List<TableCellElement>> rows = [];
+  var index = 2;
 
+  void boolToText() {
+
+  }
   //function to create table
   void printGameBlueprint(row, col) {
-    List<List<TableCellElement>> rows = [];
 
     for (var i = 0; i < row; i++) {
       List<TableCellElement> cells = [];
       for (var f = 0; f < col; f++) {
-        var cell = TableCellElement()..text = '□ ';
-        cells.add(cell);
+        var cell = TableCellElement()..text = '□';
+        cells.add(cell); // tworzy rzad
       }
       rows.add(cells);
-    }
+    } //wypelnianie tablicy pusto
 
     var table = TableElement();
     for (var i = 0; i < row; i++) {
@@ -26,13 +30,23 @@ void main() {
       table.append(tableRow);
     }
     querySelector('#gameDiv')?.append(table); // dodanie
+  } // dodawanie tablicy do div
+
+  void updateCellText() {
+    for (var i = 0; i < rows.length; i++) {
+      for (var j = 0; j < rows[i].length; j++) {
+        var cell = rows[i][j];
+        // if (cell) {
+          
+        // }
+      }
+    }
+    print(rows);
+    print('Wykonuję co 1 sekundy!');
   }
 
-
-  Timer.periodic(Duration(seconds: 2), (timer) {
-    print('Wykonuję co 2 sekundy!');
-    print(timer);
-  });
-
   printGameBlueprint(20, 60);
+  Timer.periodic(Duration(seconds: 1), (timer) {
+      updateCellText();
+  });
 }
