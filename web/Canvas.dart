d43b29881f1a40d3af4c7a8333a88c38f739654b
Adrian Z <sylzyg555@interia.pl>
2024-02-07 16:33:36 +0100

    commit

--- web/Canvas.dart
+++ web/Canvas.dart
@@ -2,46 +2,44 @@ import 'dart:async';
 import 'dart:html';
 import 'dart:math';
 
-
-
-  void drawChart() {
+void drawChart() {
   CanvasElement canvas = CanvasElement(width: window.innerWidth, height: 400);
   querySelector("#containerForCanvas")!.append(canvas);
   CanvasRenderingContext2D context = canvas.context2D;
- 
-
+  var isPaused = true;
+  querySelector('#startCanvas')?.onClick.listen((event) {
+    isPaused = !isPaused;
+  });
   Timer.periodic(Duration(milliseconds: 800), (timer) {
-   
-  int numPoints = Random().nextInt(200) + 120;
-   List<Point> data = List.generate(
+    int numPoints = Random().nextInt(200) + 120;
+    List<Point> data = List.generate(
       numPoints,
       (index) => Point(0, Random().nextInt(330) + 50),
     );
-  var distance = window.innerWidth! / data.length;
+    var distance = window.innerWidth! / data.length;
 
+    void draw() {
+      context.clearRect(0, 0, canvas.width!, canvas.height!);
 
-   void draw() {
-    context.clearRect(0, 0, canvas.width!, canvas.height!);
+      context.beginPath();
+      context.moveTo(0, data[0].y);
 
-    context.beginPath();
-    context.moveTo(0, data[0].y);
+      for (var i = 1; i < data.length - 1; i++) {
+        var newDistance = distance * i;
+        context.lineTo(newDistance, data[i].y);
+        context.arc(newDistance + 3, data[i].y + 5, 1, 0, 2 * pi);
+        context.stroke();
+      }
 
-    for (var i = 1; i < data.length-1; i++) {
-      var newDistance = distance * i;
-      context.lineTo(newDistance, data[i].y);
-      context.arc(newDistance+3, data[i].y+5, 1, 0, 2 * pi);
+      context.lineTo(canvas.width!, data[data.length - 1].y);
+      context.lineTo(canvas.width!, canvas.height!);
+      context.lineTo(0, canvas.height!);
+      context.fillStyle =
+          'rgb(${Random().nextInt(255)}, ${Random().nextInt(255)}, ${Random().nextInt(255)})';
+      context.fill();
       context.stroke();
     }
 
-    context.lineTo(canvas.width!,data[data.length-1].y);
-    context.lineTo(canvas.width!,canvas.height!);
-    context.lineTo(0,canvas.height!);
-    context.fillStyle = 'rgb(${Random().nextInt(255)}, ${Random().nextInt(255)}, ${Random().nextInt(255)})';
-    context.fill();
-    context.stroke();
-  }
-
-  draw();
-    });
+    if (!isPaused) draw();
+  });
 }
-  
